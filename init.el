368449f90a47cfe723bed42a2fcf62a35fc2d19d
フランク <freed034@pm.me>
2023-04-04 20:17:57 +0200

    Update init file

--- init.el
+++ init.el
@@ -1,29 +1,39 @@
 ;;====================================================================
-;; Created by フランク <freed034@pm.me> at Caen - France, February 2015
-;; Last modification made at Orsay - France, December 2021
-;;===================================================================
+;; Created by フランク <freed034@pm.me> at Caen - France
+;; Modifications made at Orsay - France
+;;====================================================================
 
-(setq DIR "~/Repos/GLab/Emacs/")
+(setq user-DIR "~/Repos/GLab/Emacs/")
+(setq user-emacs-directory "~/.config/emacs/")
 
 (require 'package)
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
+(setq package-enable-at-startup nil)
 (package-initialize)
-
 (when (not package-archive-contents) (package-refresh-contents))
+
+;;===================================================================
+;; Verify use-package
+;;===================================================================
+
+;; use-package to simplify the config file
+(unless (package-installed-p 'use-package) (package-refresh-contents) (package-install 'use-package))
+(setq use-package-verbose t)
+;; (setq use-package-always-ensure t)
+(require 'use-package)
 
 ;;===================================================================
 ;; Reload emacs configuration
 ;;===================================================================
 
 ;; Function to reload the init file
-(defun reload-init-file () (interactive) (load-file "~/.emacs.d/init.el"))
+(defun freed034/reload-init-file () (interactive) (load-file (concat user-DIR "init.el")))
 
 ;; Shorcut for reload the init file
-(global-set-key (kbd "\C-c l") 'reload-init-file)
+(global-set-key (kbd "\C-c l") 'freed034/reload-init-file)
 
 ;;===================================================================
 ;; Server emacs
@@ -32,27 +42,27 @@
 ;;(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
 
 ;; Function to save buffers, quit and shutdown emacs server instance
-(defun server-shutdown () (interactive) (save-some-buffers) (kill-emacs))
+(defun freed034/server-shutdown () (interactive) (save-some-buffers) (kill-emacs))
 
-(global-set-key (kbd "\C-c \C-k \C-r") 'server-shutdown)
+(global-set-key (kbd "\C-c \C-k \C-r") 'freed034/server-shutdown)
 
 ;;===================================================================
 ;; Environment variables from shell
 ;;===================================================================
 
-(require 'exec-path-from-shell)
-(exec-path-from-shell-initialize)
+(use-package exec-path-from-shell
+  :init (exec-path-from-shell-initialize))
 
 ;;===================================================================
 ;; My functions for recognize the operative system host
 ;;===================================================================
 
-(defun system-is-mac () (interactive) (string-equal system-type "darwin"))
+(defun freed034/system-is-mac () (interactive) (string-equal system-type "darwin"))
 
-(defun system-is-linux () (interactive) (string-equal system-type "gnu/linux"))
+(defun freed034/system-is-linux () (interactive) (string-equal system-type "gnu/linux"))
 
-;; Use lsb_release
-(defun which-linux-distribution ()
+;; This function makes use of lsb_release
+(defun freed034/which-linux-distribution ()
   (interactive)
   ;; (when (eq system-type 'gnu/linux) (shell-command-to-string "lsb_release -sd")) ;; distro with version
   (when (eq system-type 'gnu/linux) (replace-regexp-in-string
@@ -63,36 +73,31 @@
 ;; Load configurations based in the operating system host
 ;;===================================================================
 
-(if (system-is-linux)
+(if (freed034/system-is-linux)
     (progn
-      (when (string-equal (which-linux-distribution) 'Arch)
-        (setq custom-file (concat DIR "custom_arch.el"))
-        (setq my_config (concat DIR "conf_arch.el")))
-      (when (string-equal (which-linux-distribution) 'Ubuntu)
-        (setq custom-file (concat DIR "custom_ubun.el"))
-        (setq my_config (concat DIR "conf_ubun.el")))))
+      (when (string-equal (freed034/which-linux-distribution) 'Arch)
+        (setq custom-file (concat user-DIR "custom_arch.el"))
+        (setq my_config (concat user-DIR "config_arch.el"))
+	(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e"))
+      (when (string-equal (freed034/which-linux-distribution) 'Ubuntu)
+        (setq custom-file (concat user-DIR "custom_ubuntu.el"))
+        (setq my_config (concat user-DIR "config_ubuntu.el")))))
 
-(if (system-is-mac)
+(if (freed034/system-is-mac)
     (progn
-      (setq custom-file (concat DIR "custom_macos.el"))
-      (setq my_config (concat DIR "conf_macos.el"))))
+      (setq custom-file (concat user-DIR "custom_macos.el"))
+      (setq my_config (concat user-DIR "config_macos.el"))
+      ;; The exact next path may differ, check it!
+      (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu/mu4e")))
 
-;; First, I load the custom-file for install the packages
+;; Load the custom-file for customizations and the packages' list
 (load custom-file)
 
 ;; Installing packages
 (package-install-selected-packages)
-;; (unless (package-installed-p 'auctex) (package-install 'auctex))
 
+;; Load the config-file
 (load-file my_config)
-
-;;===================================================================
-;; Path for the manually download packages
-;;===================================================================
-
-;;(add-to-list 'load-path (expand-file-name "~/.elisp/"))
-;;(add-to-list 'load-path (concat DIR "packages"))
-;;(setq manual-directory (concat DIR "packages"))
 
 ;;===================================================================
 ;; Define word bindings
