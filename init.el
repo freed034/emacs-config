8a9d33397b4dba64e5fc380473f50ae8b804e3ca
フランク <freed034@pm.me>
2023-04-11 05:03:06 +0200

    Update init and README file

--- init.el
+++ init.el
@@ -1,65 +1,82 @@
-;;====================================================================
-;; Created by フランク <freed034@pm.me> at Caen - France
+;; Created by Frank Taipe (フランク) <freed034@pm.me> at Caen - France
 ;; Modifications made at Orsay - France
+;; URL: https://github.com/freed034/emacs-config
+
+;;====================================================================
+;; Initial variables
 ;;====================================================================
 
-(setq user-DIR "~/Repos/GLab/Emacs/")
-(setq user-emacs-directory "~/.config/emacs/")
+(setq user-DIR (concat (getenv "HOME") "/Repos/GLab/Emacs/"))
+(setq user-emacs-directory (concat (getenv "HOME") "/.config/emacs/"))
+
+;;====================================================================
+;; Set the package manager
+;;====================================================================
 
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
+                         ;; ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (setq package-enable-at-startup nil)
 (package-initialize)
-(when (not package-archive-contents) (package-refresh-contents))
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
-;;===================================================================
-;; Verify use-package
-;;===================================================================
+;;====================================================================
+;; Use-package
+;;====================================================================
 
-;; use-package to simplify the config file
-(unless (package-installed-p 'use-package) (package-refresh-contents) (package-install 'use-package))
+;; use-package to simplify the config of each package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 (setq use-package-verbose t)
 ;; (setq use-package-always-ensure t)
-(require 'use-package)
 
-;;===================================================================
-;; Reload emacs configuration
-;;===================================================================
+;;====================================================================
+;; Reload emacs config
+;;====================================================================
 
 ;; Function to reload the init file
-(defun freed034/reload-init-file () (interactive) (load-file (concat user-DIR "init.el")))
+(defun freed034/reload-init-file ()
+  (interactive)
+  (load-file (concat user-DIR "init.el")))
 
 ;; Shorcut for reload the init file
 (global-set-key (kbd "\C-c l") 'freed034/reload-init-file)
 
-;;===================================================================
+;;====================================================================
 ;; Server emacs
-;;===================================================================
-
-;;(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
+;;====================================================================
 
 ;; Function to save buffers, quit and shutdown emacs server instance
-(defun freed034/server-shutdown () (interactive) (save-some-buffers) (kill-emacs))
+(defun freed034/server-shutdown ()
+  (interactive)
+  (save-some-buffers)
+  (kill-emacs))
 
 (global-set-key (kbd "\C-c \C-k \C-r") 'freed034/server-shutdown)
 
-;;===================================================================
+;;====================================================================
 ;; Environment variables from shell
-;;===================================================================
+;;====================================================================
 
 (use-package exec-path-from-shell
-  :init (exec-path-from-shell-initialize))
+  :init (exec-path-from-shell-initialize)
+  )
 
-;;===================================================================
-;; My functions for recognize the operative system host
-;;===================================================================
+;;====================================================================
+;; Functions to recognize the os host
+;;====================================================================
 
-(defun freed034/system-is-mac () (interactive) (string-equal system-type "darwin"))
+(defun freed034/system-is-mac ()
+  (interactive)
+  (string-equal system-type "darwin"))
 
-(defun freed034/system-is-linux () (interactive) (string-equal system-type "gnu/linux"))
+(defun freed034/system-is-linux ()
+  (interactive)
+  (string-equal system-type "gnu/linux"))
 
 ;; This function makes use of lsb_release
 (defun freed034/which-linux-distribution ()
@@ -69,39 +86,41 @@
                                      "Description:\\|[\t\n\r]+" ""
                                      (shell-command-to-string "lsb_release --id --short"))))
 
-;;===================================================================
-;; Load configurations based in the operating system host
-;;===================================================================
+;;====================================================================
+;; Set configs and variables based in the os host
+;;====================================================================
 
 (if (freed034/system-is-linux)
     (progn
       (when (string-equal (freed034/which-linux-distribution) 'Arch)
-        (setq custom-file (concat user-DIR "custom_arch.el"))
-        (setq my_config (concat user-DIR "config_arch.el"))
-	(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e"))
+        (setq custom-file (concat user-DIR "custom_arch.el")
+              config-file (concat user-DIR "config_arch.el")
+	      mu4e-path-var "/usr/local/share/emacs/site-lisp/mu4e"))
       (when (string-equal (freed034/which-linux-distribution) 'Ubuntu)
-        (setq custom-file (concat user-DIR "custom_ubuntu.el"))
-        (setq my_config (concat user-DIR "config_ubuntu.el")))))
+        (setq custom-file (concat user-DIR "custom_ubuntu.el")
+              config-file (concat user-DIR "config_ubuntu.el")))))
 
 (if (freed034/system-is-mac)
-    (progn
-      (setq custom-file (concat user-DIR "custom_macos.el"))
-      (setq my_config (concat user-DIR "config_macos.el"))
-      ;; The exact next path may differ, check it!
-      (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu/mu4e")))
+      (setq custom-file (concat user-DIR "custom_macos.el")
+            config-file (concat user-DIR "config_macos.el")
+            ;; The exact next path may differ, check it!
+            mu4e-path-var "/usr/local/share/emacs/site-lisp/mu/mu4e"))
 
-;; Load the custom-file for customizations and the packages' list
-(load custom-file)
+;; Load the custom-file for customizations and the package list
+(load-file custom-file)
 
 ;; Installing packages
 (package-install-selected-packages)
 
-;; Load the config-file
-(load-file my_config)
+;; Load the path for mu4e
+(add-to-list 'load-path mu4e-path-var)
 
-;;===================================================================
-;; Define word bindings
-;;===================================================================
+;; Load the config-file with all the package configs
+(load-file config-file)
+
+;;====================================================================
+;; Bindings for define word
+;;====================================================================
 
 (global-set-key (kbd "\C-c d") 'define-word-at-point)
 (global-set-key (kbd "\C-c D") 'define-word)
